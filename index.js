77c178bd1e176a6302e621947e207b71f89decf4
Gino Guerra <ginoiguerra@gmail.com>
2023-10-10 16:30:24 -0400

    cred safety

---
+++ index.js
@@ -0,0 +1,20 @@
+//import tools form library
+//import { Client } from 'pg'
+import pg from 'pg'
+const { Client } = pg
+// import connection string from credentials / secrets file
+import { connectionURI } from './creds.js'
+
+// create client connection to database
+const client = new Client({
+    connectionString: connectionURI,
+})
+
+
+//open connection
+await client.connect()
+// run query
+const time = await client.query('SELECT NOW()')
+console.log(time)
+//close connection
+await client.end()
